78463584191f968d8ffa4c3107e4b686a8392d44
Raksha Jaiswal <72040957+Raksha-coder@users.noreply.github.com>
2022-03-10 14:33:12 +0530

    Create for_loop.cpp

---
+++ for_loop.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+main(){
+	
+//	if(initialization; condition ; operations){
+//		
+//	}
+
+	int i =1;
+	for(;i<=10;cout<< i++ << endl){
+		
+	}
+	
+}
+
+//we can write anything in operations in for() loop
+// you can initialize value either global or in local scope

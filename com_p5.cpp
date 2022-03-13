ad2807d91a7a98be4458d51bc5cb5a85b70de02a
Raksha Jaiswal <ku.rakshadilipjaiswal@gmail.com>
2022-03-14 00:24:59 +0530

    there is new condition w!=2

---
+++ com_p5.cpp
@@ -0,0 +1,13 @@
+//even number problem 
+// source : https://www.hackerearth.com/problem/algorithm/mangoes/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+main(){
+    int w;
+    cin>>w;
+
+    (w%2==0 && w!=2)?cout<<"YES":cout<<"NO";
+    
+}
